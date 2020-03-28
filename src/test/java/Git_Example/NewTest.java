c06b1a1f105d2e7e7b41c976318c1da5d367d205
unknown <animeshbanik2018@gmail.com>
2020-03-28 22:34:01 +0530

    desc

--- src/test/java/Git_Example/NewTest.java
+++ src/test/java/Git_Example/NewTest.java
@@ -8,15 +8,19 @@ public class NewTest {
   @Test
   public void f() {
 	  System.out.println("Test");
+	  System.out.println("Test");
   }
   @BeforeTest
   public void beforeTest() {
 	  System.out.println("Before Test");
+	  System.out.println("Test");
+	  System.out.println("Test");
   }
 
   @AfterTest
   public void afterTest() {
 	  System.out.println("After Test");
+	  System.out.println("Test");
   }
 
 }
